6561b3704aec3ff5bdacfc7093cbfe63e198625d
muhamadjihad <92348649+muhamadjihad@users.noreply.github.com>
2021-11-09 19:58:39 -0800

    Add files via upload

--- config.php
+++ config.php
@@ -3,13 +3,12 @@
 $server = "localhost";
 $user = "root";
 $password = "";
-$database = "db_jihad";
+$nama_database = "pendaftaran_siswa";
 
-$db = mysqli_connect($server, $user, $password, $database);
+$db = mysqli_connect($server, $user, $password, $nama_database);
 
 if( !$db ){
-    die("Gagal terhubung dengan database: " .
-    mysqli_connect_eror());
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
 }
 
 ?>
--- hapus.php
+++ hapus.php
@@ -1,25 +1,25 @@
 <?php
+
 include("config.php");
 
-if(isset($_GET['id']) ){
-  
-    //ambil id dari query string
+if( isset($_GET['id']) ){
+
+    // ambil id dari query string
     $id = $_GET['id'];
 
-    //buat query hapus
-    $sql = "DELETE FROM mahasiswa WHERE id=$id";
+    // buat query hapus
+    $sql = "DELETE FROM calon_siswa WHERE id=$id";
     $query = mysqli_query($db, $sql);
 
-    //apakah query hapus berhasil
+    // apakah query hapus berhasil?
     if( $query ){
-        header('Location: list.php');
-    } else{
+        header('Location: list-siswa.php');
+    } else {
         die("gagal menghapus...");
     }
 
-}else {
+} else {
     die("akses dilarang...");
 }
 
 ?>
-
